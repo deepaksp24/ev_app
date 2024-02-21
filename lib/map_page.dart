830c7bdfa00c9ad332ff0b31f26390a6dddd7259
sathvik-sp <79745724+sathvik-sp@users.noreply.github.com>
2024-02-22 00:30:41 +0530

    added pin feature to selected destination and display the same on the search box

--- lib/map_page.dart
+++ lib/map_page.dart
@@ -1,9 +1,11 @@
 import 'package:ev_app/search_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:ev_app/map_utils.dart';
 import 'package:google_maps_webservice/places.dart';
+import 'package:geocoding/geocoding.dart' as geo_coding;
 
 class MapperClass extends StatefulWidget {
   const MapperClass({Key? key}) : super(key: key);
@@ -12,9 +14,12 @@ class MapperClass extends StatefulWidget {
   CurrentLocationScreenState createState() => CurrentLocationScreenState();
 }
 
+//String searchText = '';
+
 class CurrentLocationScreenState extends State<MapperClass> {
   late GoogleMapController _googleMapController;
   late GoogleMapsPlaces places;
+  final TextEditingController _destination = TextEditingController();
 
   static const CameraPosition _initialCameraPosition = CameraPosition(
     target: LatLng(13.0564640879, 77.5058428128),
@@ -38,35 +43,51 @@ class CurrentLocationScreenState extends State<MapperClass> {
             zoomControlsEnabled: false,
             mapType: MapType.normal,
             onMapCreated: (GoogleMapController controller) {
-              _googleMapController = controller;
+              _onMapCreated(controller);
             },
           ),
-          const Positioned(
+          Positioned(
             top: 30,
             right: 10,
             left: 10,
-            child: SearchBarWidget(),
+            child: SearchBarWidget(
+              destination: _destination,
+              onTap: () async {
+                String? searchposition = await showGoogleAutoComplete(context);
+
+                if (searchposition != null) {
+                  _destination.text = searchposition;
+                  List<geo_coding.Location> placemarks =
+                      await geo_coding.locationFromAddress(searchposition);
+                  Position position = Position(
+                    latitude: placemarks[0].latitude,
+                    longitude: placemarks[0].longitude,
+                    accuracy: 100.0,
+                    altitude: 0.0,
+                    heading: 0.0,
+                    speed: 0.0,
+                    speedAccuracy: 0.0,
+                    timestamp: DateTime.now(),
+                    altitudeAccuracy: 0.0,
+                    headingAccuracy: 0.0,
+                  );
+                  _moveCameraToUserLocation(position);
+                  _addMarkerToSelectedLocation(position);
+
+                  setState(() {
+                    //searchText = searchposition;
+                  });
+                }
+              },
+            ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () async {
           Position position = await determinePosition();
-
-          _googleMapController.animateCamera(CameraUpdate.newCameraPosition(
-            CameraPosition(
-              target: LatLng(position.latitude, position.longitude),
-              zoom: 14,
-            ),
-          ));
-
-          _markers.clear();
-
-          _markers.add(Marker(
-            markerId: const MarkerId('currentLocation'),
-            position: LatLng(position.latitude, position.longitude),
-          ));
-
+          _moveCameraToUserLocation(position);
+          _addMarkerToUserLocation(position);
           setState(() {});
         },
         label: const Text("Current Location"),
@@ -74,4 +95,52 @@ class CurrentLocationScreenState extends State<MapperClass> {
       ),
     );
   }
+
+  void _onMapCreated(GoogleMapController controller) {
+    _googleMapController = controller;
+  }
+
+  void _moveCameraToUserLocation(Position position) {
+    _googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        target: LatLng(position.latitude, position.longitude),
+        zoom: 16,
+      ),
+    ));
+  }
+
+  void _addMarkerToUserLocation(Position position) {
+    _markers.clear();
+    _markers.add(Marker(
+      markerId: const MarkerId('currentLocation'),
+      position: LatLng(position.latitude, position.longitude),
+    ));
+  }
+
+  void _addMarkerToSelectedLocation(Position position) {
+    _markers.clear();
+    _markers.add(Marker(
+      markerId: const MarkerId('selectedLocation'),
+      position: LatLng(position.latitude, position.longitude),
+    ));
+  }
+}
+
+Future<String?> showGoogleAutoComplete(BuildContext context) async {
+  const kGoogleApiKey = 'AIzaSyCstj5OMwmGOYOYifN4I_A-tz_qtP7iL5c';
+
+  Prediction? p = await PlacesAutocomplete.show(
+    offset: 0,
+    radius: 100000000,
+    strictbounds: false,
+    language: "en",
+    context: context,
+    mode: Mode.overlay,
+    apiKey: kGoogleApiKey,
+    components: [Component(Component.country, "in")],
+    types: [],
+    hint: "Search Hospitals",
+  );
+
+  return p?.description;
 }
--- lib/search_bar.dart
+++ lib/search_bar.dart
@@ -1,34 +1,36 @@
-// ignore_for_file: avoid_print
-
 import 'package:flutter/material.dart';
-import 'package:flutter_google_places/flutter_google_places.dart';
-import 'package:google_maps_webservice/places.dart';
 
 class SearchBarWidget extends StatelessWidget {
-  const SearchBarWidget({super.key});
+  final VoidCallback onTap;
 
+  const SearchBarWidget({
+    Key? key,
+    required this.destination,
+    required this.onTap,
+  }) : super(key: key);
+  final TextEditingController destination;
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Container(
       height: 50,
       decoration: BoxDecoration(
-          color: const Color.fromARGB(235, 255, 255, 255),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.05),
-              spreadRadius: 1,
-              blurRadius: 1,
-            ),
-          ],
-          borderRadius: BorderRadius.circular(100)),
+        color: const Color.fromARGB(235, 255, 255, 255),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.05),
+            spreadRadius: 1,
+            blurRadius: 1,
+          ),
+        ],
+        borderRadius: BorderRadius.circular(100),
+      ),
       child: Row(
         children: [
           Expanded(
             child: TextFormField(
+              controller: destination,
               readOnly: true,
-              onTap: () {
-                showGoogleAutoComplete(context);
-              },
+              onTap: onTap,
               style: const TextStyle(
                 color: Colors.black,
                 fontSize: 14,
@@ -51,25 +53,5 @@ class SearchBarWidget extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-void showGoogleAutoComplete(BuildContext context) async {
-  const kGoogleApiKey = 'AIzaSyCstj5OMwmGOYOYifN4I_A-tz_qtP7iL5c';
-
-  Prediction? p = await PlacesAutocomplete.show(
-    offset: 0,
-    radius: 100000000,
-    strictbounds: false,
-    language: "en",
-    context: context,
-    mode: Mode.overlay,
-    apiKey: kGoogleApiKey,
-    components: [Component(Component.country, "in")],
-    types: [],
-    hint: "Search Hospitals",
-  ).whenComplete(() => print('Autocomplete'));
-  if (p != null) {
-    print(p.description);
   }
 }
